abcc4c16f4159933e45d31846e7d3800cc2f51e7
AimHanif <hanifworkmy@gmail.com>
2024-11-25 14:18:10 +0800

    v1

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:barber_housecall/screens/home.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:google_fonts/google_fonts.dart'; // Import Google Fonts package
+import 'package:google_fonts/google_fonts.dart';
 import 'firebase_options.dart';
 import 'screens/customer.dart';
-import 'screens/barber.dart'; // Import BarberHomeScreen
+import 'screens/barber.dart';
 import 'services/notification_services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // Initialize Notification Service
   await NotificationService.initialize();
 
   runApp(const MyApp());
@@ -26,11 +30,11 @@ class MyApp extends StatelessWidget {
       title: 'Barber Housecall',
       theme: ThemeData(
         primarySwatch: Colors.red,
-        textTheme: GoogleFonts.jostTextTheme(), // Use Poppins font
+        textTheme: GoogleFonts.jostTextTheme(), // Apply Google Fonts
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => const HomeScreen(), // Map '/' to RootScreen
+        '/': (context) => const HomeScreen(),
         '/customer': (context) => const CustomerHomeScreen(),
         '/barber': (context) => const BarberHomeScreen(),
       },
